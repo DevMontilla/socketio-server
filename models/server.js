4aa21ce870b2850b75525e6f9a27879ee591b377
Daniel Montilla <dm.fullstackdeveloper@gmail.com>
2022-10-05 09:43:55 -0300

    First commit

---
+++ models/server.js
@@ -0,0 +1,42 @@
+//Express server
+const express = require("express");
+//Socket server
+const http = require("http");
+const socketio = require("socket.io");
+const path = require("path");
+const Sockets = require("./sockets");
+
+class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+
+    // Http server
+    this.server = http.createServer(this.app);
+
+    //Config de sockets
+    this.io = socketio(this.server, {
+      /** configuraciones */
+    });
+  }
+
+  middlewares() {
+    this.app.use(express.static(path.resolve(__dirname, "../public")));
+  }
+
+  configurarSockets() {
+    new Sockets(this.io);
+  }
+
+  execute() {
+    this.middlewares();
+
+    this.configurarSockets();
+
+    this.server.listen(this.port, () => {
+      console.log("Server running on port: ", this.port);
+    });
+  }
+}
+
+module.exports = Server;
